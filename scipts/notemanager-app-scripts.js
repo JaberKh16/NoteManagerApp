56788e5532e4f2e65610376fd13256638f28d64c
Jaber Khan <md.jaberkhan66@gmail.com>
2022-08-02 11:17:29 +0600

    NoteApp-Files

--- scipts/notemanager-app-scripts.js
+++ scipts/notemanager-app-scripts.js
@@ -17,17 +17,19 @@ document.getElementById('add-btn').addEventListener('click', function(event){
     // checking whether the input field is empty or not
     if(addInputField.value !== ''){
         // creating necessary tags for adding new note inside the unorder list element as list item
+        // though for a list as our contnet we need 1 list item, 2 paragraphs,
+        // 2 icons and 1 input item.  
         const listElement = document.createElement('li'),
             textualContentParagraph = document.createElement('p'),
             tagsContentParagraph = document.createElement('p'),
-            rightSignedIcon = document.createElement('i'),
-            crossSignedIcon = document.createElement('i'),
+            editIcon = document.createElement('i'),
+            deleteIcon = document.createElement('i'),
             inputFieldElement = document.createElement('input');
         
             
         // adding the necessary attributes for those elements
-        rightSignedIcon.className = 'fa fa-pencil-square-o';
-        crossSignedIcon.className = 'fa fa-times';
+        editIcon.className = 'fa fa-pencil-square-o';
+        deleteIcon.className = 'fa fa-times';
         inputFieldElement.className = 'edit-note';
         inputFieldElement.setAttribute('type', 'text');
 
@@ -35,8 +37,8 @@ document.getElementById('add-btn').addEventListener('click', function(event){
         textualContentParagraph.textContent = addInputField.value;  // addInputField is the textual content provided by the user
 
         // merging those created tags with list element
-        tagsContentParagraph.appendChild(rightSignedIcon); // adding the first icon to the tags based paragraph
-        tagsContentParagraph.appendChild(crossSignedIcon); // adding the second icon to the tags based paragraph
+        tagsContentParagraph.appendChild(editIcon); // adding the first icon to the tags based paragraph
+        tagsContentParagraph.appendChild(deleteIcon); // adding the second icon to the tags based paragraph
         listElement.appendChild(textualContentParagraph); // adding the textual content based paragraph into the list
         listElement.appendChild(tagsContentParagraph); // adding the tags content based paragraph into the list
         listElement.appendChild(inputFieldElement); // adding the input field for editing note into the list
